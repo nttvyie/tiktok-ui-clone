76da8cd91fc3c1ad5fb36cb198051454e69262fd
Nguyen Vy <nttvy27@gmail.com>
2023-03-22 21:07:32 +0700

    updated

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,38 @@
+import images from '~/assets/images';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                {/* LOGO SECTION */}
+                <img src={images.logo} alt="Tiktok Icon" />
+
+                {/* SEARCH SECTION */}
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                    {/* Clear logo */}
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+
+                    {/* Loading Logo */}
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    {/* Search Logo */}
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                {/* ACTION SECTION */}
+                <div className={cx('action')}></div>
+            </div>
         </header>
     );
 }
